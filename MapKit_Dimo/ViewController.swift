cdd3ddbbbf112206e514925cea4d033c28694db0
Abanoub Nabil <abanoubn91@gmail.com>
2019-01-29 18:09:31 +0200

    add camera

--- MapKit_Dimo/ViewController.swift
+++ MapKit_Dimo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var map: MKMapView!
     
     var location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
+    var camera = MKMapCamera()
     
     @IBAction func segmented(_ sender: Any) {
         
@@ -23,16 +24,22 @@ class ViewController: UIViewController {
         switch segmented.selectedSegmentIndex {
         case 0:
             location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
+            updateMapCameraWith(heading: 45.0, altitute: 1000)
         case 1:
             location = CLLocationCoordinate2DMake(30.0472733,31.2053506)
+            updateMapCameraWith(heading: 45.0, altitute: 100)
         case 2:
             location = CLLocationCoordinate2DMake(52.4456775,-1.8840589)
+            updateMapCameraWith(heading: 45.0, altitute: 50)
         case 3:
             location = CLLocationCoordinate2DMake(52.4799864,-1.8363626)
+            updateMapCameraWith(heading: 45.0, altitute: 10)
         case 4:
             location = CLLocationCoordinate2DMake(27.9881199,86.9162203)
+            updateMapCameraWith(heading: 45.0, altitute: 1000)
         default:
             location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
+            updateMapCameraWith(heading: 45.0, altitute: 1000)
         }
         
         updateMapWithRegion(distanec: 100)
@@ -48,6 +55,13 @@ class ViewController: UIViewController {
     func updateMapWithRegion(distanec: CLLocationDistance) {
         var region = MKCoordinateRegion(center: location, latitudinalMeters: distanec, longitudinalMeters: distanec)
         map.region = region
+    }
+    
+    func updateMapCameraWith(heading : CLLocationDirection , altitute : CLLocationDistance) {
+        camera.centerCoordinate = location
+        camera.heading = heading
+        camera.altitude = altitute
+        map.camera = camera
     }
 
 }
