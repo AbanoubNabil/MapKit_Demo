01ed3a2d4fa8c456e6ed1d3345196195558ec419
Abanoub Nabil <abanoubn91@gmail.com>
2019-02-27 16:31:57 +0200

    add map Type

--- MapKit_Dimo/ViewController.swift
+++ MapKit_Dimo/ViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 import MapKit
+/*
+ Naples: 40.8367321 , 14.2468856
+ New York: 40.7216294 , -73.995453
+ Chicago: 41.892479 , -87.6267592
+ Chatham: beverly Hils: 34.0674607 , -118.3977309
+ */
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var changePich: UIButton!
     
     var location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
     var camera = MKMapCamera()
+    var pich = 0
+    
+    @IBAction func mapType(_ sender: Any) {
+        switch map.mapType {
+        case .standard:
+            map.mapType = .satellite
+        case .satellite:
+            map.mapType = .hybrid
+        case .hybrid:
+            map.mapType = .satelliteFlyover
+        case .satelliteFlyover:
+            map.mapType = .hybridFlyover
+        case .hybridFlyover:
+            map.mapType = .mutedStandard
+        case .mutedStandard:
+            map.mapType = .standard
+        }
+        
+    }
+    
+    @IBAction func changePichaction(_ sender: Any) {
+        pich = (pich + 15) % 90
+        map.camera.pitch = CGFloat(pich)
+        changePich.setTitle("\(pich)ْ", for: .normal)
+    }
     
     @IBAction func segmented(_ sender: Any) {
         
@@ -23,24 +54,24 @@ class ViewController: UIViewController {
         
         switch segmented.selectedSegmentIndex {
         case 0:
-            location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
-            updateMapCameraWith(heading: 180, altitute: 1000.0)
+            location = CLLocationCoordinate2DMake(40.8367321 , 14.2468856)
+            updateMapCameraWith(heading: 180, altitute: 100.0)
             break
         case 1:
-            location = CLLocationCoordinate2DMake(30.0472733,31.2053506)
-            updateMapCameraWith(heading: 45.0, altitute: 5000.0)
+            location = CLLocationCoordinate2DMake(40.7216294 , -73.995453)
+            updateMapCameraWith(heading: 245.0, altitute: 250.0)
             break
         case 2:
-            location = CLLocationCoordinate2DMake(52.4456775,-1.8840589)
+            location = CLLocationCoordinate2DMake(41.892479 , -87.6267592)
             updateMapCameraWith(heading: 90.0, altitute: 50)
             break
         case 3:
             location = CLLocationCoordinate2DMake(52.4799864,-1.8363626)
-            updateMapCameraWith(heading: 180.0, altitute: 10)
+            updateMapCameraWith(heading: 180.0, altitute: 200)
             break
         case 4:
-            location = CLLocationCoordinate2DMake(27.9881199,86.9162203)
-            updateMapCameraWith(heading: -20.0, altitute: 1000)
+            location = CLLocationCoordinate2DMake(34.0674607 , -118.3977309)
+            updateMapCameraWith(heading: -20.0, altitute: 70)
             break
         default:
             location = CLLocationCoordinate2DMake(30.0405125,31.2174571)
@@ -66,6 +97,8 @@ class ViewController: UIViewController {
     func updateMapCameraWith(heading : CLLocationDirection , altitute : CLLocationDistance) {
         camera.centerCoordinate = location
         camera.heading = heading
+        camera.pitch = 0.0
+        changePich.setTitle("0", for: .normal)
         camera.altitude = altitute
         map.camera = camera
     }
